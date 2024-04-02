94ed61d5816b5af500c901b29ed08ee081f16111
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-04-02 11:28:32 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# reduct-vs-mongodb
-Benchmarks for MongoDB and ReductStore in Python
+# Benchmarks for MongoDB and ReductStore in Python
+
+This repository contains a simple benchmark for MongoDB and ReductStore in Python. The benchmark consists of inserting binary data into respective databases and measuring the time taken to insert and retrieve the data.
+
+## Running
+
+```bash
+pip install -r requirements.txt
+docker-compose up -d
+python main.py
+```
+
+## Results
+
+The following table shows the average time taken to insert and retrieve binary data of different sizes into MongoDB and ReductStore.
+
+| Chunk Size | Operation | MongoDB, blob/s | ReductStore, blob/s | ReductStore, %  |
+|------------|-----------|-----------------|---------------------|-----------------|
+| 10 KB      | Write     | 258             | 223                 | -14             |
+|            | Read      | 187             | 195                 | 4               |
+| 100 KB     | Write     | 145             | 197                 | 36              |
+|            | Read      | 108             | 145                 | 34              |
+| 1 MB       | Write     | 32              | 52                  | 63              |
+|            | Read      | 20              | 29                  | 45              |
+| 10 MB      | Write     | 4               | 7                   | 75              |
+|            | Read      | 2               | 4                   | 100             |
+
+
+## Ressources
+
+- [MongoDB](https://www.mongodb.com/)
+- [MongoDB Time Series Manual](https://www.mongodb.com/docs/manual/core/timeseries/timeseries-procedures/)
+- [MongoDB Large Objects](https://www.mongodb.com/developer/products/mongodb/storing-large-objects-and-files/)
+- [ReductStore](https://www.reduct.store/)
---
+++ main.py
@@ -0,0 +1,115 @@
+from datetime import datetime
+
+from pymongo import MongoClient
+from gridfs import GridFS
+
+import random
+import time
+import asyncio
+
+from reduct import Client as ReductClient
+
+BLOB_SIZE = 10_000
+BLOB_COUNT = min(1000, 1_000_000_000 // BLOB_SIZE)
+
+CHUNK = random.randbytes(BLOB_SIZE)
+
+HOST = "localhost"
+CONNECTION_MONGODB = f"mongodb://admin:password@{HOST}:27017/benchmark?authSource=admin"
+CONNECTION_REDUCT = f"http://{HOST}:8383"
+
+
+def setup_database():
+    with MongoClient(CONNECTION_MONGODB) as client:
+        db = client["benchmark"]
+        if "data" not in db.list_collection_names():
+            db.create_collection("data", timeseries={"timeField": "time"})
+
+
+def write_to_mongodb():
+    setup_database()
+
+    with MongoClient(CONNECTION_MONGODB) as client:
+        db = client["benchmark"]
+        fs = GridFS(db)
+        data = db["data"]
+        count = 0
+        for _ in range(BLOB_COUNT):
+            blob_id = fs.put(CHUNK, filename=f"blob_{datetime.now().timestamp()}")
+            data.insert_one({"time": datetime.now(), "blob_id": blob_id})
+            count += BLOB_SIZE
+
+    return count
+
+
+def read_from_mongodb(t1, t2):
+    count = 0
+    with MongoClient(CONNECTION_MONGODB) as client:
+        db = client["benchmark"]
+        fs = GridFS(db)
+        data = db["data"]
+        for rec in data.find(
+            {
+                "time": {
+                    "$gt": datetime.fromtimestamp(t1),
+                    "$lt": datetime.fromtimestamp(t2),
+                }
+            }
+        ):
+            blob = fs.get(rec["blob_id"]).read()
+            count += len(blob)
+
+    return count
+
+
+async def write_to_reduct():
+    async with ReductClient(
+        CONNECTION_REDUCT, api_token="reductstore"
+    ) as reduct_client:
+        count = 0
+        bucket = await reduct_client.get_bucket("benchmark")
+        for _ in range(BLOB_COUNT):
+            await bucket.write("data", CHUNK)
+            count += BLOB_SIZE
+
+        return count
+
+
+async def read_from_reduct(t1, t2):
+    async with ReductClient(
+        CONNECTION_REDUCT, api_token="reductstore"
+    ) as reduct_client:
+        count = 0
+        bucket = await reduct_client.get_bucket("benchmark")
+        async for rec in bucket.query("data", t1, t2, ttl=90):
+            count += len(await rec.read_all())
+
+        return count
+
+
+if __name__ == "__main__":
+    print(f"Chunk size={BLOB_SIZE / 1000_000} Mb, count={BLOB_COUNT}")
+    ts = time.time()
+    size = write_to_mongodb()
+    print(
+        f"Write {size / 1000_000} Mb to MongoDB: {BLOB_COUNT / (time.time() - ts)} req/s"
+    )
+
+    ts_read = time.time()
+    size = read_from_mongodb(ts, time.time())
+    print(
+        f"Read {size / 1000_000} Mb from MongoDB: {BLOB_COUNT / (time.time() - ts)} req/s"
+    )
+
+    loop = asyncio.new_event_loop()
+    ts = time.time()
+    size = loop.run_until_complete(write_to_reduct())
+    print(
+        f"Write {size / 1000_000} Mb to ReductStore: {BLOB_COUNT / (time.time() - ts)} req/s"
+    )
+
+    ts_read = time.time()
+    size = loop.run_until_complete(read_from_reduct(ts, time.time()))
+    print(
+        f"Read {size / 1000_000} Mb from ReductStore: {BLOB_COUNT / (time.time() - ts)} req/s"
+    )
